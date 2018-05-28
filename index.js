9adb76a1dc684947d34e2c374afe95b065dd19e5
Arun Wamanrao Wadikar <arunw@cybage.com>
2018-05-28 16:09:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function calculateCircumference(radius) {
+    return 2 * Math.PI * radius;
+  }
+  
+  console.log(Math.PI);
+  // expected output: 3.141592653589793
+  
+  console.log(calculateCircumference(10));
+
+  module.exports = {
+    calculateCircumference:calculateCircumference
+  }
